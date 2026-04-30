c2b5097ac8249ca0a396980fb8099c5493dfae35
Hans Schenker <gian.schenker@gmail.com>
2026-04-30 11:27:04 +0200

    feat: add SidebarSlice, PanelSlice, sidebarState$, panelState$ to tree.state

--- rxjs-decision-tree/src/state/tree.state.ts
+++ rxjs-decision-tree/src/state/tree.state.ts
@@ -1,8 +1,8 @@
 // src/state/tree.state.ts
-import { Subject, scan, startWith, shareReplay } from 'rxjs'
+import { Subject, scan, startWith, shareReplay, map, distinctUntilChanged } from 'rxjs'
 import { ROOT } from '../tree/tree.config'
 import { makeReducer } from './tree.reducer'
-import type { Action, TreeState } from '../tree/tree.types'
+import type { Action, TreeState, TreeNode, BreadcrumbStep, DetailView } from '../tree/tree.types'
 
 export const action$ = new Subject<Action>()
 
@@ -19,4 +19,37 @@ export const state$ = action$.pipe(
 	scan(treeReducer, initial),
 	startWith(initial),
 	shareReplay(1),
+)
+
+export interface SidebarSlice {
+	currentNode: TreeNode
+	breadcrumb:  BreadcrumbStep[]
+}
+
+export interface PanelSlice {
+	currentNode: TreeNode
+	breadcrumb:  BreadcrumbStep[]
+	historyLen:  number
+	detailView:  DetailView | null
+}
+
+export const sidebarState$ = state$.pipe(
+	map(({ currentNode, breadcrumb }): SidebarSlice => ({ currentNode, breadcrumb })),
+	distinctUntilChanged((a, b) =>
+		a.currentNode === b.currentNode && a.breadcrumb === b.breadcrumb
+	),
+)
+
+export const panelState$ = state$.pipe(
+	map(({ currentNode, breadcrumb, history, detailView }): PanelSlice => ({
+		currentNode,
+		breadcrumb,
+		historyLen: history.length,
+		detailView,
+	})),
+	distinctUntilChanged((a, b) =>
+		a.currentNode === b.currentNode &&
+		a.historyLen  === b.historyLen  &&
+		a.detailView  === b.detailView
+	),
 )
