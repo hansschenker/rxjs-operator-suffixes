1c227631c5fd7cd1eef7602f28f724eadda58adb
Hans Schenker <gian.schenker@gmail.com>
2026-05-14 07:43:06 +0200

    feat: scaffold marble-rxjs-todos project

---
+++ marblejs/marblejs-jozef-flakus/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  esbuild: {
+    jsxFactory: 'h',
+    jsxFragment: 'null',
+  },
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        rewrite: path => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
